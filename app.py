17e68605cb65d609b2a1e5e99cad141e2cc32260
aks2123 <44139244+aks2123@users.noreply.github.com>
2024-05-06 21:20:39 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from prediction import predict
+import numpy as np
+
+st.title ('Insurance predictor')
+st.write ('Please select SAT score and press the ***Predict GPA*** button')
+
+
+sat = st.number_input ('select', min_value= 400, max_value= 1600)
+
+if st.button ('Predict GPA'):
+    GPA = predict ([[sat]])
+    st.write ('GPA would be', GPA)
