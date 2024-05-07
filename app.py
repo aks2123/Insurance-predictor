44dd189c8da46c78479b1b480f70cd8e29a7bf05
aks2123 <44139244+aks2123@users.noreply.github.com>
2024-05-06 21:28:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ import numpy as np
 st.title ('Insurance predictor')
 st.write ('Please select age')
 a = st.number_input ('select', min_value= 0, max_value= 120)
+st.write ('Please select gender: 0 for male and 1 for female')
+b = st.number_input ('select', min_value= 0, max_value= 1)
+st.write ('Please select bmi')
+c = st.number_input ('select', min_value= 0, max_value= 70)
+st.write ('Please select children')
+d = st.number_input ('select', min_value= 0, max_value= 20)
+st.write ('Please select smoking status: 0 for smoker, 1 for non-smoker ')
+e = st.number_input ('select', min_value= 0, max_value= 1)
 
 if st.button ('Predict GPA'):
     GPA = predict ([[sat]])
