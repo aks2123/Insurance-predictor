6be807401db3b526dafeb59bd13d3798209d33cf
aks2123 <44139244+aks2123@users.noreply.github.com>
2024-05-06 22:53:29 -0500

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -2,9 +2,8 @@ import streamlit as st
 import pickle
 import numpy as np
 
-with open ('model.pkl', 'rb') as f:
+def predict (data):
+  with open ('model.pkl', 'rb') as f:
 
     model = pickle.load (f)
-
-def predict (data):
     return model.predict (data)
